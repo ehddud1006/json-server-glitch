28a2ea6a0bb5d3af6894a14642e6960f6029d92d
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-03-11 00:45:18 +0900

    feat: json 서버 라우트 설정
    feat: routes test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    tset
    
    feat: json 서버 라우트 설정

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router(require("./db.js")());
+const data = require("./db.js")();
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-server.use(router);
+
+server.get("/boards", async (req, res) => {
+  console.log(data.boards);
+  res.status(200).json(data.boards);
+});
 
 server.listen(port);
